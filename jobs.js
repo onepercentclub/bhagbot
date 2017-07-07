6a52078afc0093714810ef35c5c6118e9de5bc28
Maurice Kleine <info@mauricekleine.nl>
2017-07-07 15:06:32 +0200

    final

--- jobs.js
+++ jobs.js
@@ -1,12 +1,12 @@
 const currentWeekNumber = require('current-week-number');
 const schedule = require('node-schedule');
 
-const crewChannel = 'C63PW9UGM'; // C63PW9UGM for test channel, C02A2JZQY for crew channel, C4CF2GA91 for team-engineering
+const crewChannel = 'C02A2JZQY'; // C63PW9UGM for test channel, C02A2JZQY for crew channel, C4CF2GA91 for team-engineering
 
 module.exports = (bot, controller, influx) => {
   // Ask question on Friday
-  const friday = { dayOfWeek: 5, hour: 15, minute: 0 };
-  schedule.scheduleJob({ dayOfWeek: 5, hour: 11, minute: 30 }, () => {
+  const friday = { dayOfWeek: 5, hour: 15, minute: 15 };
+  schedule.scheduleJob(friday, () => {
     bot.api.channels.info({ channel: crewChannel }, (err, { channel }) => {
       channel.members.map(userId => {
         bot.api.users.info({ user: userId }, (err, { user }) => {
